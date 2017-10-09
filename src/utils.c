35301152ae73c40ac812e8f84fe3c8da872d0ca8
Jeongwu Song <jws5346@naver.com>
2017-10-09 23:30:36 +0900

    submit

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,33 @@
 #include "utils.h"
+#include <string.h>
+#include <stdlib.h>
 
 void mysh_parse_command(const char* command,
                         int *argc, char*** argv)
 {
-  // TODO: Fill this!
+    *argv  = (char**)malloc(sizeof(*argv)*256);
+    *argc = 0;
+    char *tmp;
+
+    tmp = strtok(strdup(command), " \t\n"); 
+
+	if(tmp == NULL){
+	*argc = 1;
+	**argv = malloc(10);
+	strcpy((**argv),"");
+	(*argv)++;
+	}
+
+    while(tmp!=NULL)
+    {
+        **argv = malloc(strlen(tmp) + 1);
+        strcpy(**argv,tmp);
+        (*argv)++;
+        (*argc)++;
+        tmp = strtok(NULL, " \t\n");
+    }
+    
+
+    *argv -= (*argc);
+    *argv = (char**)realloc(*argv, sizeof(*argv)*(*argc));
 }
